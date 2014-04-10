0da9f9c2e6b046a6383d0bef339305affb4cf315
Sayak Sarkar <sayak.bugsmith@gmail.com>
2014-04-10 17:17:13 +0530

    Logical code optimization.

--- scripts/app.js
+++ scripts/app.js
@@ -8,6 +8,88 @@ app.controller("AppCtrl", function ($http, $scope) {
         document.getElementById("header").className = 'step';
         var app = this;
 
+        displayCharts = function () {
+            document.getElementById("pieChart").innerHTML = "";
+            document.getElementById("count").innerHTML = "";
+            document.getElementById("languages").innerHTML = "";
+
+            $http.get("https://api.github.com/users/" + $scope.formUsernameText + "/repos?per_page=9999").success(function (data) {
+                var languages = [];
+                var count = [];
+                var position = 0,
+                    index;
+                for (var item in data) {
+                    if (data[item].language !== null) {
+                        index = languages.indexOf(data[item].language);
+                        if (index === -1) {
+                            languages[position] = data[item].language;
+                            count[position] = 1;
+                            position++;
+                        } else {
+                            count[index]++;
+                        }
+                    }
+                }
+
+                //Create Pie Chart
+                var r = new Raphael("pieChart");
+                r.text(150, 50, "Language Usage Statistics").attr({
+                    font: "20px sans-serif"
+                });
+                var pie = r.piechart(100, 150, 75, count, {
+                    legend: languages,
+                    legendpos: "east"
+                });
+                pie.hover(function () {
+                    this.sector.stop();
+                    this.sector.scale(1.1, 1.1, this.cx, this.cy);
+
+                    if (this.label) {
+                        this.label[0].stop();
+                        this.label[0].attr({
+                            r: 7.5
+                        });
+                        this.label[1].attr({
+                            "font-weight": 800
+                        });
+                    }
+                }, function () {
+                    this.sector.animate({
+                        transform: 's1 1 ' + this.cx + ' ' + this.cy
+                    }, 100, "bounce");
+
+                    if (this.label) {
+                        this.label[0].animate({
+                            r: 5
+                        }, 100, "bounce");
+                        this.label[1].attr({
+                            "font-weight": 400
+                        });
+                    }
+                });
+
+                document.getElementById("count").innerHTML = "<strong>Number of Languages:</strong> " + position;
+                document.getElementById("repoStats").innerHTML = "<strong>Number of Repositories per Language:-</strong>";
+                var langdata, value, datanode, valuenode, node;
+                for (var language in languages) {
+                    node = document.createElement("tr");
+                    langdata = document.createElement("td");
+                    value = document.createElement("td");
+                    datanode = document.createTextNode(languages[language]);
+                    langdata.appendChild(datanode);
+                    valuenode = document.createTextNode(count[language].value);
+                    value.appendChild(valuenode);
+                    node.appendChild(langdata);
+                    node.appendChild(value);
+                    document.getElementById("languages").appendChild(node);
+                }
+            }).error(function () {
+                document.getElementById("intermediate").className = 'hidden';
+                document.getElementById("header").className = 'initial';
+                alert("Hmm.... That doesn't look quite right!\n\nOctoStats couldn't find the User's data on GitHub! :-/");
+            });
+        };
+
         $http.get("https://api.github.com/users/" + $scope.formUsernameText).success(function (data) {
             app.user = data;
 
@@ -63,93 +145,11 @@ app.controller("AppCtrl", function ($http, $scope) {
 
             //Call Chart Display Function
             displayCharts();
+
         }).error(function () {
             document.getElementById("intermediate").className = 'hidden';
             document.getElementById("header").className = 'initial';
             alert("Hmm.... That doesn't look quite right!\n\nOctoStats couldn't find the User's data on GitHub! :-/");
         });
-
-        displayCharts = function () {
-            document.getElementById("pieChart").innerHTML = "";
-            document.getElementById("count").innerHTML = "";
-            document.getElementById("languages").innerHTML = "";
-
-            $http.get("https://api.github.com/users/" + $scope.formUsernameText + "/repos?per_page=9999").success(function (data) {
-                var languages = [];
-                var count = [];
-                var position = 0,
-                    index;
-                for (var item in data) {
-                    if (data[item].language !== null) {
-                        index = languages.indexOf(data[item].language);
-                        if (index === -1) {
-                            languages[position] = data[item].language;
-                            count[position] = 1;
-                            position++;
-                        } else {
-                            count[index]++;
-                        }
-                        //console.log(data[item].language);
-                    }
-                }
-
-                //Create Pie Chart
-                var r = new Raphael("pieChart");
-                r.text(150, 50, "Language Usage Statistics").attr({
-                    font: "20px sans-serif"
-                });
-                pie = r.piechart(100, 150, 75, count, {
-                    legend: languages,
-                    legendpos: "east"
-                });
-                pie.hover(function () {
-                    this.sector.stop();
-                    this.sector.scale(1.1, 1.1, this.cx, this.cy);
-
-                    if (this.label) {
-                        this.label[0].stop();
-                        this.label[0].attr({
-                            r: 7.5
-                        });
-                        this.label[1].attr({
-                            "font-weight": 800
-                        });
-                    }
-                }, function () {
-                    this.sector.animate({
-                        transform: 's1 1 ' + this.cx + ' ' + this.cy
-                    }, 100, "bounce");
-
-                    if (this.label) {
-                        this.label[0].animate({
-                            r: 5
-                        }, 100, "bounce");
-                        this.label[1].attr({
-                            "font-weight": 400
-                        });
-                    }
-                });
-
-                document.getElementById("count").innerHTML = "<strong>Number of Languages:</strong> " + position;
-                document.getElementById("repoStats").innerHTML = "<strong>Number of Repositories per Language:-</strong>";
-                var data, value, datanode, valuenode, node;
-                for (var item in languages) {
-                    node = document.createElement("tr");
-                    data = document.createElement("td");
-                    value = document.createElement("td");
-                    datanode = document.createTextNode(languages[item]);
-                    data.appendChild(datanode);
-                    valuenode = document.createTextNode(count[item].value);
-                    value.appendChild(valuenode);
-                    node.appendChild(data);
-                    node.appendChild(value);
-                    document.getElementById("languages").appendChild(node);
-                }
-            }).error(function () {
-                document.getElementById("intermediate").className = 'hidden';
-                document.getElementById("header").className = 'initial';
-                alert("Hmm.... That doesn't look quite right!\n\nOctoStats couldn't find the User's data on GitHub! :-/")
-            })
-        }
-    }
-})
+    };
+});
